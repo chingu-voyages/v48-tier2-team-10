0fe02e80554f1cb3c7ad7563dabf4aa92afe7464
jess <120062852+jessabc@users.noreply.github.com>
2024-03-19 19:22:48 +1100

    add usecontext to dinosaur page

--- src/pages/DinosaurPage.jsx
+++ src/pages/DinosaurPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { DinoDataContext } from "../context/DinoDataContext";
 
 export default function DinosaurPage() {
   const { dino } = useParams();
+
+  // is this dinoData needed from useContext, or is the data needed passed through props? just putting it here for now in case...
+  const { dinoData } = useContext(DinoDataContext);
+
   return <div>Dinosaur Page - {dino}</div>;
 }
