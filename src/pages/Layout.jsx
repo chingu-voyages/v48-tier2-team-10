09ca516af301b79d29c061305f8e537f591e17b5
Jo Sanchez <shvinjas@gmail.com>
2024-03-26 00:53:57 +1300

    Revert "charts updated v1"
    This reverts commit 91ca7bd757e7ea598a4906a6f54e4dc963878ea9.

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -3,14 +3,12 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/shared/Header/Header";
 import Footer from "../components/shared/Footer/Footer";
 import { DinoDataContextProvider } from "../context/DinoDataContext";
-import Chart from "./Chart";
 
 export default function Layout() {
   return (
     <DinoDataContextProvider>
       <Header />
       <Outlet />
-      <Chart />
       <Footer />
     </DinoDataContextProvider>
   );
