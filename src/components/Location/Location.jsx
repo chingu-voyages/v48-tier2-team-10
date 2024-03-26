646e5fa674f68e585c195bf1ec3cde0f9aecdb9e
jess <120062852+jessabc@users.noreply.github.com>
2024-03-26 11:58:34 +1100

    add location component

---
+++ src/components/Location/Location.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Location() {
+  return (
+    <div>
+      <p>location</p>
+      {/* <p>{a}</p> */}
+    </div>
+  );
+}
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,14 +1,20 @@
-import { useContext } from 'react'
-import { DinoDataContext } from '../context/DinoDataContext'
+import { useContext } from "react";
+import { DinoDataContext } from "../context/DinoDataContext";
+import Location from "../components/Location/Location";
 
 export default function Landing() {
-  const { dinoData, loading, error } = useContext(DinoDataContext)
+  const { dinoData, loading, error } = useContext(DinoDataContext);
 
-  console.log(dinoData)
+  console.log(dinoData);
 
-  if (loading) return <p>loading...</p>
+  if (loading) return <p>loading...</p>;
 
-  if (error) return <p>cannot fetch dinosaurs</p>
+  if (error) return <p>cannot fetch dinosaurs</p>;
 
-  return <div>Landing</div>
+  return (
+    <>
+      <h1>landing</h1>
+      <Location />
+    </>
+  );
 }
