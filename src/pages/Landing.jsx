f51f11f7f77b0c80c00d4fbb7030707e7ff0e6c9
jess <120062852+jessabc@users.noreply.github.com>
2024-03-28 20:08:05 +1100

    add LinksDrawer

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,19 +1,46 @@
-import { useContext } from 'react'
-import { DinoDataContext } from '../context/DinoDataContext'
-import Hero from '../components/Hero'
+import { useContext } from "react";
+import { DinoDataContext } from "../context/DinoDataContext";
+import Hero from "../components/Hero";
+import Location from "../components/Location/Location";
+import LinksDrawer from "../components/Location/LinksDrawer/LinksDrawer";
+import { useState } from "react";
+import Chart from "./Chart";
+import styles from "./Landing.module.css";
+import { getCountries } from "../helpers/getCountries";
 
 export default function Landing() {
-  const { dinoData, loading, error } = useContext(DinoDataContext)
+  const [isLinksModalOpen, setIsLinksModalOpen] = useState(false);
+  const [country, setCountry] = useState();
 
-  // console.log(dinoData)
+  const { dinoData, loading, error } = useContext(DinoDataContext);
 
-  if (loading) return <p>loading...</p>
+  console.log(dinoData);
 
-  if (error) return <p>cannot fetch dinosaurs</p>
+  if (loading) return <p>loading...</p>;
+
+  if (error) return <p>cannot fetch dinosaurs</p>;
+
+  const toggleModal = (dino) => {
+    setCountry(dino.position.country);
+    setIsLinksModalOpen(true);
+  };
 
   return (
     <div>
       <Hero />
+
+      <Chart />
+
+      <div className={styles.locationContainer}>
+        <Location dinoData={getCountries(dinoData)} toggleModal={toggleModal} />
+      </div>
+
+      <LinksDrawer
+        toggleModal={toggleModal}
+        isLinksModalOpen={isLinksModalOpen}
+        setIsLinksModalOpen={setIsLinksModalOpen}
+        country={country}
+      />
     </div>
-  )
+  );
 }
