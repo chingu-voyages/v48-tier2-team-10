e87d92d9c475efc98a773eada1bd0ddf646a9d09
jess <120062852+jessabc@users.noreply.github.com>
2024-03-28 20:07:14 +1100

    tidy

--- src/pages/DinosaurPage.jsx
+++ src/pages/DinosaurPage.jsx
@@ -1,11 +1,20 @@
-import { useParams } from 'react-router-dom'
-import { DinoDataContext } from '../context/DinoDataContext'
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { DinoDataContext } from "../context/DinoDataContext";
+import Location from "../components/Location/Location";
 
 export default function DinosaurPage() {
-  const { dino } = useParams()
+  const { dino } = useParams();
 
-  // is this dinoData needed from useContext, or is the data needed passed through props? just putting it here for now in case...
-  const { dinoData } = useContext(DinoDataContext)
+  const { dinoData } = useContext(DinoDataContext);
 
-  return <div>Dinosaur Page - {dino}</div>
+  const thisDino = dinoData.find((dinosaur) => dinosaur.name === dino);
+
+  return (
+    <>
+      <div>Dinosaur Page - {dino}</div>
+
+      <Location dinoData={[thisDino]} />
+    </>
+  );
 }
