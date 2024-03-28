4d4155da0b7dedc978eed0aa0b2b3ca367d48508
jess <120062852+jessabc@users.noreply.github.com>
2024-03-28 20:07:37 +1100

    comment out chart

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -9,8 +9,10 @@ export default function Layout() {
   return (
     <DinoDataContextProvider>
       <Header />
+
       <Outlet />
-      <Chart/>
+
+      {/* <Chart /> */}
       <Footer />
     </DinoDataContextProvider>
   );
