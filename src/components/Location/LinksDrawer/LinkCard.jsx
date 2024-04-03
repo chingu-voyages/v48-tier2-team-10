86dd32fa4a58ce06c43ea9e3cc0b21b019832532
jess <120062852+jessabc@users.noreply.github.com>
2024-04-03 19:48:45 +1100

    link card

---
+++ src/components/Location/LinksDrawer/LinkCard.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import styles from "./LinkCard.module.css";
+import noImage from "../../../assets/no-dino-img.png";
+
+export default function LinkCard({ dino }) {
+  return (
+    <div className={styles.cardContainer}>
+      <Link to={`/dinosaurs/${dino.name}`} className={styles.link}>
+        <div className={styles.imageContainer}>
+          {dino.imageSrc === "N/A" ? (
+            <img src={noImage} alt="no dino image" />
+          ) : (
+            <img
+              src={dino.imageSrc}
+              alt={`${dino.name} image`}
+              className={styles.dinoImg}
+            />
+          )}
+        </div>
+        <div className={styles.nameContainer}>
+          <span className={styles.name}>{dino.name}</span>
+        </div>
+      </Link>
+    </div>
+  );
+}
