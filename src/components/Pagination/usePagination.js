1d912b1f1e8860d8e248cc4b140d983264cb4d6c
jess <120062852+jessabc@users.noreply.github.com>
2024-04-03 19:48:26 +1100

    pagination

---
+++ src/components/Pagination/usePagination.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function usePagination(itemsPerPage, searchResults) {
+  // Here we use item offsets; we could also use page offsets
+  // following the API or data you're working with.
+  const [itemOffset, setItemOffset] = useState(0);
+
+  // Simulate fetching items from another resources.
+  // (This could be items from props; or items loaded in a local state
+  // from an API endpoint with useEffect and useState)
+  const endOffset = itemOffset + itemsPerPage;
+  // console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentItems = searchResults?.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(searchResults?.length / itemsPerPage);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % searchResults?.length;
+    // console.log(
+    //   `User requested page number ${event.selected}, which is offset ${newOffset}`
+    // );
+    setItemOffset(newOffset);
+  };
+
+  return {
+    currentItems,
+    pageCount,
+    handlePageClick,
+    setItemOffset,
+    itemOffset,
+  };
+}
