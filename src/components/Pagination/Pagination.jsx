a61662fd8c5478bfa2cbecba204c1389a5df7386
jess <120062852+jessabc@users.noreply.github.com>
2024-04-03 13:58:31 +1100

    carat

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,27 +1,61 @@
 import ReactPaginate from "react-paginate";
+import styles from "./Pagination.module.css";
+import carat from "../../assets/carat.svg";
 
 export default function Pagination({ handlePageClick, pageCount }) {
+  const prev = (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M15.1599 7.41L10.5799 12L15.1599 16.59L13.7499 18L7.74991 12L13.7499 6L15.1599 7.41Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+  const next = (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      transform="rotate(180)"
+    >
+      <path
+        d="M15.1599 7.41L10.5799 12L15.1599 16.59L13.7499 18L7.74991 12L13.7499 6L15.1599 7.41Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+
   return (
     <>
       <ReactPaginate
-        nextLabel="next >"
+        nextLabel={next}
         onPageChange={handlePageClick}
         pageRangeDisplayed={3}
         marginPagesDisplayed={2}
         pageCount={pageCount}
-        previousLabel="< previous"
-        pageClassName="page-item"
-        pageLinkClassName="page-link"
-        previousClassName="page-item"
-        previousLinkClassName="page-link"
-        nextClassName="page-item"
-        nextLinkClassName="page-link"
+        previousLabel={prev}
+        pageClassName={styles.pageItem}
+        pageLinkClassName={styles.pageLink}
+        previousClassName={styles.pageItem}
+        previousLinkClassName={`${styles.pageLink} ${styles.prev}`}
+        nextClassName={styles.pageItem}
+        nextLinkClassName={`${styles.pageLink} ${styles.next}`}
         breakLabel="..."
-        breakClassName="page-item"
-        breakLinkClassName="page-link"
-        containerClassName="pagination"
-        activeClassName="active"
+        breakClassName={styles.pageItem}
+        breakLinkClassName={styles.pageLink}
+        containerClassName={styles.pagination}
+        activeClassName={styles.active}
         renderOnZeroPageCount={null}
+        disabledClassName={styles.disabled}
+        disabledLinkClassName={styles.disabled}
       />
     </>
   );
