7333be535abc637b6dc1281baeb3156c10e66ac1
jess <120062852+jessabc@users.noreply.github.com>
2024-04-04 15:16:17 +1100

    dummy style card

--- src/components/DisplaySearchResults/DisplaySearchResults.jsx
+++ src/components/DisplaySearchResults/DisplaySearchResults.jsx
@@ -1,10 +1,19 @@
-// import styles from "./DisplaySearchResults.module.css";
+import styles from "./DisplaySearchResults.module.css";
 
 export default function DisplaySearchResults({ currentItems }) {
   return (
-    <div>
+    <div className={styles.displayContainer}>
       {currentItems &&
-        currentItems.map((dino, index) => <li key={index}>{dino.name}</li>)}
+        currentItems.map((dino, index) => (
+          // to be changed to a dino card
+          <li key={index} className={styles.cardContainer}>
+            <div>
+              <p>{dino.name}</p>
+              <p>weight: {dino.weight}</p>
+              <p>length: {dino.length}</p>
+            </div>
+          </li>
+        ))}
     </div>
   );
 }
