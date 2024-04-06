a5054fa72b5f15fab413b262c0171b9054046e64
jess <120062852+jessabc@users.noreply.github.com>
2024-04-06 16:36:26 +1100

    404

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import styles from "./NoPage.module.css";
+import { Link } from "react-router-dom";
 
 export default function NoPage() {
-  return <div>404</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading}>404</h1>
+      <p>Oops, something went wrong.</p>
+      <p>Sorry, we couldn't find your page.</p>
+
+      <Link to={"/"} className={styles.button}>
+        Home
+      </Link>
+    </div>
+  );
 }
