8b7ff69f519780c505a8cac2dcad4173468ee86d
jess <120062852+jessabc@users.noreply.github.com>
2024-04-06 14:34:24 +1100

    n/a bug fix

--- src/components/filter/checkbox/useLookUp.js
+++ src/components/filter/checkbox/useLookUp.js
@@ -16,11 +16,12 @@ export default function useLookUp(checkboxes) {
     below100kg: "Below 100kg",
     Above100kgBelow1000kg: "100kg-1000kg",
     higherThan1000kg: "Higher than 1000kg",
-    NAWeight: "N/A",
+
     below1m: "Below 1m",
     Above1mBelow10m: "1m-10m",
     higherThan10m: "Higher than 10m",
     NALength: "N/A",
+    NAWeight: "N/A",
     ...countriesLookUp,
   };
 
